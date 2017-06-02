da2d6fcba5d553dda2fec30cb654d6e8ff8e2ec1
selvaramesh <ram.sundhar97@gmail.com>
2017-06-02 15:33:37 +0530

    Create oddeven.java

---
+++ oddeven.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+class GFG {
+	public static void main(String[] args)
+	{
+		Scanner a=new Scanner(System.in);
+	   long int b=a.nextInt();
+	   if (b%2==0)
+	   {
+	       System.out.println("the number is even");
+	   }
+	   else
+	   {
+	       System.out.println("the number is odd");
+	   }
+	}
+}
