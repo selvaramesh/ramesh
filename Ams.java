ed4a6ebcb544827c6a6f49266cd6250b427a5e45
selvaramesh <ram.sundhar97@gmail.com>
2017-06-09 18:10:00 +0530

    Create Ams.java

---
+++ Ams.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+class Ams {
+	public static void main (String[] args) 
+	{
+	 Scanner sc=new Scanner(System.in);
+	 int n=sc.nextInt();
+	 int d=(int) Math.log10(n)+1;
+	 int c=0;
+	 int r=0;
+	 int temp=n;
+	 while(n>0)
+	 {
+	     r=n%10;
+	     n=n/10;
+	     c+=Math.pow(r,d);
+	 }
+	 System.out.println(c);
+	 if(c==temp)
+	 {
+	     System.out.println("amstrong number");
+	 }
+	 else{
+	     System.out.println("not an amstrong number");
+	 }
+	}
+}
