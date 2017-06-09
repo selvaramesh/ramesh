1592362cd28d0d4479bab48c087d5b6b65c5a97e
selvaramesh <ram.sundhar97@gmail.com>
2017-06-09 09:34:40 +0530

    Create Sreverse.java

---
+++ Sreverse.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+class Sreverse {
+	public static void main (String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    String s=sc.nextLine();
+	    StringBuffer sb=new StringBuffer(s);
+	    sb.reverse();
+	    System.out.println(sb);
+	
+	}
+}
