c895f69df13ef05959edfadca40663f44142363d
selvaramesh <ram.sundhar97@gmail.com>
2017-06-12 15:21:42 +0530

    Create Hunter53.java

--- Hunter53.java
+++ Hunter53.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-class GFG {
+class Hunter53 {
 	public static void main (String[] args) {
 	    Scanner sc=new Scanner(System.in);
 	    String s=sc.nextLine();
