b5b7e4c73da5bda15c6b14847a2e4b3fda7c9c58
selvaramesh <ram.sundhar97@gmail.com>
2017-06-12 22:09:54 +0530

    Create Hunter60.java

--- Hunter60.java
+++ Hunter60.java
@@ -1,18 +1,18 @@
-import java.io.*;
 import java.util.*;
-
-class GFG {
+public class Hunter60 {
 	public static void main (String[] args) {
 	    Scanner sc=new Scanner (System.in);
 	    int n=sc.nextInt();
-	    int a[]=new int[n];int c=0;
-	    int b[]=new int[n];
+            int m=sc.nextInt();
+	    int a[]=new int[n];
+	    int b[]=new int[m];
+            int flag=0;
 	    for(int i=0;i<n;i++)
 	    {
 	        a[i]=sc.nextInt();
 	    }
 	    Arrays.sort(a);
-	    for(int i=0;i<n;i++)
+	    for(int i=0;i<m;i++)
 	    {
 	        b[i]=sc.nextInt();
 	    }
@@ -20,8 +20,9 @@ class GFG {
 	    {
 	        if(a[0]==b[i])
 	        {
-	            c=c+1;
+	            flag=i;
 	        }
-	    }
+            }
+            System.out.println(flag-1);
 	}
 }
