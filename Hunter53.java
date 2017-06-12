7078d9be58d23bc35e7cec665e77c299d201c6ca
selvaramesh <ram.sundhar97@gmail.com>
2017-06-12 15:21:13 +0530

    Create Hunter53.java

---
+++ Hunter53.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+class GFG {
+	public static void main (String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    String s=sc.nextLine();
+	    int d=sc.nextInt();
+	    int n=s.length();
+	    char ch[]=s.toCharArray();
+	    for(int i=0;i<d-1;i++)
+	    {
+	        for(int j=i+1;j<n;j++)
+	       { 
+	       System.out.print(ch[i]);
+	       System.out.println(ch[j]);
+	        
+	    }
+	    }
+	}
+}
