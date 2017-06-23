c6bf7aa1dcac34e4b7c1b1bf20c04efb10469c18
selvaramesh <ram.sundhar97@gmail.com>
2017-06-23 14:31:41 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class hello
+{
+    public static void main(String args[])
+    {
+        for(int i=0;i<5;i++)
+        {
+            System.out.println("hello");
+        }
+    }
+}
