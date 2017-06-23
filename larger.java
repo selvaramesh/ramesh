f0e35b4374c10b6ad396dbdb7d9157ab85fa400c
selvaramesh <ram.sundhar97@gmail.com>
2017-06-23 14:12:39 +0530

    Create larger.java

---
+++ larger.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class larger
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        System.out.println(Math.max(a,(Math.max(b,c))));
+    }
+}
